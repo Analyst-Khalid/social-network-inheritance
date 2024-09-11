0d3d594a17414466ed05c5ba3e19c450abfdd1b4
Analyst-Khalid <leaderkhalidsa@gmail.com>
2024-09-11 12:52:24 +0300

    Update accounts.py

--- social_network/accounts.py
+++ social_network/accounts.py
@@ -1,13 +1,21 @@
-
-class User(object):
+class User:
     def __init__(self, first_name, last_name, email):
-        pass
+        self.first_name = first_name  
+        self.last_name = last_name    
+        self.email = email            
+        self.posts = []               
+        self.following = []          
 
     def add_post(self, post):
-        pass
+        post.set_user(self)  
+        self.posts.append(post)  
 
     def get_timeline(self):
-        pass
+        return sorted(self.posts, key=lambda post: post.timestamp, reverse=True)
 
     def follow(self, other):
-        pass
+        if other not in self.following:
+            self.following.append(other)  
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
