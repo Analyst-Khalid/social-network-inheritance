db5cbabc4e4f33fac348812212740d40ad6c50f3
Analyst-Khalid <leaderkhalidsa@gmail.com>
2024-09-11 12:30:29 +0300

    Update posts.py

--- social_network/posts.py
+++ social_network/posts.py
@@ -1,36 +1,40 @@
 from datetime import datetime
 
-# Please remove the comments and 
-# create these classes as it corresponds:
-# (your tests will fail if you don't comment out these classes)
+class Post(object):
+    def __init__(self, text, timestamp=None):
+        self.text = text
+        self.timestamp = timestamp or datetime.now()
 
-# class Post(object):
-#     def __init__(self, text, timestamp=None):
-#         pass
-#
-#     def set_user(self, user):
-#         pass
+    def set_user(self, user):
+        self.user = user
 
-
-# class TextPost(...):  # Inherit properly
-#     def __init__(self, text, timestamp=None):
-#         pass
-#
-#     def __str__(self):
-#         pass
+    def __str__(self):
+        return "{}: {} ({})".format(self.user.first_name, self.text, self.timestamp)
 
 
-# class PicturePost(...):  # Inherit properly
-#     def __init__(self, text, image_url, timestamp=None):
-#         pass
-#
-#     def __str__(self):
-#         pass
+class TextPost(Post):
+    def __init__(self, text, timestamp=None):
+        super().__init__(text, timestamp)
+
+    def __str__(self):
+        return "TextPost: {}".format(super().__str__())
 
 
-# class CheckInPost(...):  # Inherit properly
-#     def __init__(self, text, latitude, longitude, timestamp=None):
-#         pass
-#
-#     def __str__(self):
-#         pass
+class PicturePost(Post):
+    def __init__(self, text, image_url, timestamp=None):
+        super().__init__(text, timestamp)
+        self.image_url = image_url
+
+    def __str__(self):
+        return "PicturePost: {} (Image: {})".format(super().__str__(), self.image_url)
+
+
+class CheckInPost(Post):
+    def __init__(self, text, latitude, longitude, timestamp=None):
+        super().__init__(text, timestamp)
+        self.latitude = latitude
+        self.longitude = longitude
+
+    def __str__(self):
+        return "CheckInPost: {} (Location: {}, {})".format(super().__str__(), self.latitude, self.longitude)
+
